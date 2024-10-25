c532f1ae58b8444eabe97698bf84ab1e923ec75f
Gaurav Raikwar <gauravjb16@gmail.com>
2024-10-25 10:20:34 +0530

    minimul flask app

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 """
 creating a flask app
 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "hello world"
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
